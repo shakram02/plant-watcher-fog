9d7eab46ec5caf2ae9961033dc8f7bdf512f2660
shakram02 <ahmedhamdyau@gmail.com>
2020-12-01 06:01:32 +0000

    use select() and check connectivity timeouts

--- server.py
+++ server.py
@@ -1,7 +1,9 @@
 import socket
 import struct
 import uuid
+import select
 import paho.mqtt.client as mqtt
+from datetime import datetime
 MQTT_BROKER = "174.138.103.162"
 MQTT_TOPIC = "/hello_world"
 MQTT_PORT = 3881
@@ -35,53 +37,101 @@ def mqtt_main():
         client.publish(MQTT_TOPIC, message)
 
 
-def read_data(client):
+def read_data(client: socket.socket):
     data = b''
-    buf_size = 32
-    while True:
-        buf = client.recv(buf_size)
-        data += buf
-        if b"\r\n" in data:
-            break
-        elif not data:
-            break
+    buf_size = 256
+    try:
+        while True:
+            buf = client.recv(buf_size)
+            data += buf
+            if b"\r\n" in data:
+                break
+    except BlockingIOError:
+        print("BLOCKING IO", data)
+        pass
 
     return data
+
+
+def read_chunkable_message(client_buffer: bytes):
+    splits = client_buffer.split(b'\r\n')
+    if not splits:
+        return None
+
+    return splits
 
 
 def http_main():
 
     # Create a TCP/IP socket
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     addr = ("0.0.0.0", 8000)
-    sock.setsockopt(socket.SOL_SOCKET, socket.SO_LINGER, NOLINGER)
-    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    sock.setsockopt(socket.SOL_SOCKET, socket.SO_RCVTIMEO, TIMEOUT_VAL)
-    sock.setblocking(True)
-    sock.bind(addr)
-    sock.listen()
+    server_sock.setsockopt(socket.SOL_SOCKET, socket.SO_LINGER, NOLINGER)
+    server_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    server_sock.setsockopt(socket.SOL_SOCKET, socket.SO_RCVTIMEO, TIMEOUT_VAL)
+    server_sock.setblocking(0)
+    server_sock.bind(addr)
+    server_sock.listen(5)
+
+    inputs = [server_sock]
+    outputs = []
+    message_queues = {}
+    heartbeats = {}
 
     print("Waiting for connection...")
     while True:
-        try:
-            client, address = sock.accept()
-            print("Connection from {}:{}".format(*address))
-            secret = client.recv(128)
-            device_uuid = str(uuid.uuid3(NAMESPACE, str(secret)))
-            print("UUID:", device_uuid)
-            client.send(bytes(device_uuid, "UTF-8"))
-        except BlockingIOError:
-            continue
+        readable, writable, exceptional = select.select(
+            inputs, outputs, inputs)
+        print("SELECT")
+        for s in readable:
+            if s is server_sock:
+                client, address = server_sock.accept()
+                print("Connection from {}:{}".format(*address))
 
-        try:
-            while True:
-                data = read_data(client)
-                if not data:
-                    break
-                print("CLIENT:", str(data, "UTF-8").strip())
+                client.setblocking(0)
+                inputs.append(client)
+            else:
+                client: socket.socket = s
+                if client not in message_queues:
+                    secret = client.recv(128)
+                    device_uuid = str(uuid.uuid3(NAMESPACE, str(secret)))
+                    print("UUID:", device_uuid)
+                    client.send(bytes(device_uuid, "UTF-8"))
+                    message_queues[client] = b''
+                    heartbeats[client] = datetime.now()
+                else:
+                    data = read_data(client)
+                    if not data:
+                        print("Client disconnected")
+                        inputs.remove(client)
+                        outputs.remove(client)
 
-        except (BlockingIOError, TimeoutError):
-            print("Client disconnected")
+                    message_queues[client] += data
+                    msgs = read_chunkable_message(message_queues[client])
+                    if not msgs:
+                        continue
+                    else:
+                        *full, rem = msgs
+                        # Re-append unparsed messages
+                        message_queues[client] = rem
+                        print("MESSAGES:", full)
+
+            if exceptional:
+                print("EXCEPTION:", exceptional)
+
+            now = datetime.now()
+            for client in tuple(heartbeats):
+                last_heartbeat_timestamp = heartbeats[client]
+                idle_minutes = (
+                    now - last_heartbeat_timestamp).total_seconds() // 60
+                if idle_minutes > 0:
+                    print(client.getsockname(), "Idle for:",
+                          idle_minutes, "minutes")
+
+                    # Remove client data
+                    heartbeats.pop(client)
+                    inputs.remove(client)
+                    client.close()
 
 
 def main():
